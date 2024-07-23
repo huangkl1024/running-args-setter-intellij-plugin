8f485ff72210e7fd58c4aa0ef6778d977e3821c0
GitHub Action <action@github.com>
2024-07-23 03:49:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/huangkl1024/runningargssetterintellijplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/huangkl1024/runningargssetterintellijplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.huangkl1024.runningargssetterintellijplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
